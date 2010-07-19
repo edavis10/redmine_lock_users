3dfaf68c333cba2ca77c52d85c916eb75b7921b9
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-19 13:50:50 -0700

    Adding plugin docs

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_lock_users do
-  name 'Redmine Lock Users plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Lock Users'
+  author 'Eric Davis'
+  description 'Adds a Rake task to lock all Redmine users'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
